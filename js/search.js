f0c5731a34a10d743fdea505987612692d47c7e4
cconur <98234762+cconur@users.noreply.github.com>
2025-07-01 13:59:59 +0200

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
   $("#myInput").on("keyup", function() {
     var value = $(this).val().toLowerCase();
-    $("#loop .project-name").filter(function() {
-      $(this).parent().parent().toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    $("#loop .breadcrumb").filter(function() {
+      $(this).parent().parent().parent().parent().toggle($(this).text().toLowerCase().indexOf(value) > -1)
     });
   });
 });
