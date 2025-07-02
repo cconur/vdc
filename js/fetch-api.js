ba5e591318906b682bf2b03d1d1074a8f2c174a7
cconur <98234762+cconur@users.noreply.github.com>
2025-07-02 14:18:11 +0200

    Update fetch-api.js

--- js/fetch-api.js
+++ js/fetch-api.js
@@ -721,6 +721,9 @@ $(document).ready(function() {
             var seccion = param1Value.split('-')[1];
             console.log(seccion);
 
+            console.log($.cookie('count'));
+            console.log($.cookie('events'));
+
             if(seccion == "s08") {
                 var restore = $('<div class="container px-3 mt-0 restoreDiv"><div class="col text-center"><i class="mb-3 fa-solid fa-bounce fa-lg fa-angles-down"></i><div class="wrap"><button class="btn w-50 backToMain"><i class="fa-solid fa-house me-2"></i>Ir a la web de la exposición</button></div></div></div>');
                 $("#portfolio").append(restore);
@@ -740,18 +743,19 @@ $(document).ready(function() {
             let eventsValue = $('<li class="event" data-date="'+time+'"><h4 class="mb-3">Visita número: '+count+'</h4><p>Visitaste la pieza: '+param1Value+'</p></li>');
             $(".bd-journey .modal-body .timeline-1").html(eventsValue);
             $.cookie('events', eventsValue);
+            count = count + 1;
+            $.cookie('count', count);
             }
 
             else {
             let count = $.cookie('count');
             let eventsValue = $.cookie('events') + $('<li class="event" data-date="'+time+'"><h4 class="mb-3">Visita número: '+count+'</h4><p>Visitaste la pieza: '+param1Value+'</p></li>');
             $(".bd-journey .modal-body .timeline-1").html(eventsValue);
-            }
-            
             $.cookie('events', eventsValue);
             count = count + 1;
             $.cookie('count', count);
-
+            }
+            
             console.log($.cookie('count'));
             console.log($.cookie('events'));
             
