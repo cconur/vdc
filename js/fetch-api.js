70c53abe214ee96c6e5f054b66e0e3e4765e4dc2
cconur <98234762+cconur@users.noreply.github.com>
2025-08-28 13:46:56 +0200

    Update fetch-api.js

--- js/fetch-api.js
+++ js/fetch-api.js
@@ -1179,7 +1179,7 @@ $(document).ready(function(){
                 var piezaRep = $(this).text();
                 //console.log(piezaRep+" "+item);
                 if(piezaRep == item) {
-                    $(this).parent().parent().append('<span><i class="bi bi-repeat mx-2"></i>Visita repetida</span><i>'+NpiezasRepetidas+'</i> veces</span>');
+                    $(this).parent().parent().append('<span><i class="bi bi-repeat mx-2"></i>Visita repetida: </span><i>'+NpiezasRepetidas+'</i> veces</span>');
                     $(this).css("background-color", "orange");
                 }
 
